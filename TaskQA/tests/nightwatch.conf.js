8091cd2dd4978abd87254c519151c41046618321
wanuus <82518131+wanuus@users.noreply.github.com>
2024-10-27 21:45:24 +0300

    Update nightwatch.conf.js

--- TaskQA/tests/nightwatch.conf.js
+++ TaskQA/tests/nightwatch.conf.js
@@ -1,18 +1,25 @@
+const chromedriver = require('chromedriver');
+
 module.exports = {
-    src_folders: ['tests'],
-    webdriver: {
-      start_process: true,
-      server_path: require('geckodriver').path, // Make sure this points to chromedriver
-      port: 9515, // This should match the port Nightwatch expects
-    },
+    src_folders: ['tests'], // Specify the folder where your test files are located
     test_settings: {
-      default: {
-        desiredCapabilities: {
-          browserName: 'chrome',
-          chromeOptions: {
-            args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage'],
-          },
+        default: {
+            // Configuration for the webdriver
+            webdriver: {
+                start_process: true, // Set to true to start the ChromeDriver process automatically
+                server_path: chromedriver.path, // Path to the ChromeDriver binary
+                port: 9515, // The port ChromeDriver listens on
+            },
+            desiredCapabilities: {
+                browserName: 'chrome', // Specify the browser to use
+                chromeOptions: {
+                    args: [
+                        '--headless', // Run in headless mode
+                        '--no-sandbox', // Disable the sandbox for CI environments
+                        '--disable-dev-shm-usage', // Overcome limited resource problems
+                    ],
+                },
+            },
         },
-      },
     },
-  };
+};
